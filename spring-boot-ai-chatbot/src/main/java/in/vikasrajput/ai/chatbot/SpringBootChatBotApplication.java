28a43b3d6a7d108aaf882191050b7e296c51564b
vkhaas.rajput1993@gmail.com <Vikas Rajput>
2024-08-28 18:20:14 +0530

    Initial commit for chatbot

---
+++ spring-boot-ai-chatbot/src/main/java/in/vikasrajput/ai/chatbot/SpringBootChatBotApplication.java
@@ -0,0 +1,13 @@
+package in.vikasrajput.ai.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChatBotApplication.class, args);
+	}
+
+}
